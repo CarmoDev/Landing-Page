855598ad968264042e246e112af4d173b6a93ff6
CarmoDev <dalvatechnologies@gmail.com>
2023-03-21 08:35:11 -0300

    Reviews

--- src/Components/Reviews/index.tsx
+++ src/Components/Reviews/index.tsx
@@ -5,12 +5,12 @@ const Mocks = [
   {
     id: 1,
     name: "RVS",
-    review: "Lorem ipsum dolor sit amet consectetur. Enim ac justo sem viverra at convallis duis semper. Ullamcorper ut donec sollicitudin purus praesent duis. Tristique mauris sit dolor id massa ac nunc. Facilisis vulputate vel morbi turpis elementum."
+    review: "Atendimento impecável, bastante atencioso nas solicitações e entrega rápida do projeto foram os diferenciais desse trabalho."
   },
   {
     id: 2,
     name: "IMÃ",
-    review: "Lorem ipsum dolor sit amet consectetur. Venenatis feugiat ultrices posuere eleifend ultrices lacus molestie pellentesque elementum. Vulputate habitant pretium ipsum velit feugiat elementum est gravida in. Ut aliquet risus risus a. Amet commodo neque vitae nunc. Ipsum nec fusce tristique pulvinar mauris laoreet vitae at. Etiam ac cras id eu praesent porttitor dolor. Leo massa vivamus leo donec faucibus proin. Nisi tortor lacus eget in enim aliquam tincidunt. Justo egestas ornare aenean dui maecenas a lobortis. Iaculis netus leo ut consequat risus. Ac facilisi cras consectetur pharetra congue cras duis. Laoreet luctus turpis adipiscing suspendisse lacus lorem quis."
+    review: " Projeto exatamente como a empresa precisava e atendimento de qualidade! "
   },
 ];
 
